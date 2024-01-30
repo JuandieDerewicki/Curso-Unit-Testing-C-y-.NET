195205a7e31961efb14ee5265a22efc9f7f474dc
JuandieDerewicki <jdderewicki@gmail.com>
2024-01-30 19:27:28 -0300

    Realizacion de metodo de prueba de FromRomanToNumbers con theory indata y actualizaciones de metodos del palindromo y quantintywords para hacer el metodo con theory y indata

--- StringManipulationTests/StringOperationTest.cs
+++ StringManipulationTests/StringOperationTest.cs
@@ -49,6 +49,27 @@ namespace StringManipulationTests
             Assert.False(result);
         }
 
+        // PRUEBA DE ISPALINDROME CON THEORY Y INLINEDATA
+        [Theory]
+
+        [InlineData("oro", true)]
+
+        [InlineData("hello", false)]
+
+        public void IsPalindromeTest(string word, bool expected)
+        {
+            var strOperations = new StringOperations();
+            var result = strOperations.IsPalindrome(word);
+            if (expected)
+            {
+                Assert.True(result);
+            }
+            else
+            {
+                Assert.False(result);
+            }
+        }
+
         [Fact]
         public void RemoveWhitespace()
         {
@@ -72,6 +93,28 @@ namespace StringManipulationTests
             Assert.Contains("cat", result);
         }
 
+        // PRUEBA DE QUANTINTYWORDS CON THEORY Y INLINEDATA
+        [Theory]
+
+        [InlineData("car", 10, "ten cars")]
+
+        [InlineData("country", 4, "four countries")]
+
+        [InlineData("dog", 1, "one dog")]
+
+        [InlineData("tree", 100, "one hundred trees")]
+
+        public void QuantityInWordsTest(string input, int quantity, string expected)
+        {
+            // Arrange
+            var operations = new StringOperations();
+            // Act
+            var result = operations.QuantintyInWords(input, quantity);
+            //Assert
+            Assert.NotEmpty(result);
+            Assert.Equal(expected, result);
+        }
+
 
         [Fact]
         public void GetStringLenght_Exception() // Indicamos el exception pq es lo que queremos probar 
@@ -91,8 +134,20 @@ namespace StringManipulationTests
             // La estructura Act no se utiliza porque no alcanzamos a guardar nada, ya que, llamamos a la funcion y va a saltar una excepion inmediantamente 
             // Assert
             Assert.ThrowsAny<ArgumentOutOfRangeException>(() => strOperations.TruncateString("Platzi", -2));
-
         }
 
-    }
+        [Theory]
+        [InlineData("V", 5)] // De esta manera pasamos por parámetro y podemos hacer múltiples de estas comprobaciones
+        // Las pruebas unitarias generalmente no reciben parametros sino que se configura intermanente pero gracias a unos atributos que tiene Xunit se puede hacer sin usar Fact y usando Theory
+        public void FromRomanToNumbers(string romanNumer, int expected) // se agregan estos parametros para pasar el numero romano y el resultado esperado de la funcion
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+            // Act 
+            var result = strOperations.FromRomanToNumber(romanNumer); // recibe el parametro
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+    } 
 }
