46f19d2db7d6bf43010b06e166ee835bf9ad5476
JuandieDerewicki <jdderewicki@gmail.com>
2024-01-30 17:22:18 -0300

    Creacion de clases para realizar las pruebas, descarga de paquetes Xunit y realización de primera prueba unitaria sobre concatenar strings

---
+++ StringManipulationTests/StringOperationTest.cs
@@ -0,0 +1,24 @@
+﻿using StringManipulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StringManipulationTests
+{
+    public class StringOperationTest
+    {
+
+        [Fact]
+        public void ConcatenateStrings() // Las clases y metodos de prueba deben ser públicos
+        {
+            var strOperations = new StringOperations(); // Crear objeto de la clase que tiene las operaciones que queremos probar
+
+            var result = strOperations.ConcatenateStrings("Hello", "Platzi"); // Guardamos resultado de la operacion y le pasamos los strings
+
+            Assert.Equal("Hello Platzi", result); // Comprobamos que la función esté devolviendo bien con Assert nos permite hacer esa comparacion, nos dice cual es el string esperado y cual es el string actual
+        }
+    }
+}
