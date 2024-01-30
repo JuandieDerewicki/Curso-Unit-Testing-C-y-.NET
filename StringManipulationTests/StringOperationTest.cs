f3b9b49fcd154ee49a0b157c6a34bc30c06583c7
JuandieDerewicki <jdderewicki@gmail.com>
2024-01-30 17:27:31 -0300

    Agregacion de comprobaciones extra al metodo de concatenar strings

--- StringManipulationTests/StringOperationTest.cs
+++ StringManipulationTests/StringOperationTest.cs
@@ -14,10 +14,14 @@ namespace StringManipulationTests
         [Fact]
         public void ConcatenateStrings() // Las clases y metodos de prueba deben ser públicos
         {
+            // Arrange
             var strOperations = new StringOperations(); // Crear objeto de la clase que tiene las operaciones que queremos probar
-
+            // Act
             var result = strOperations.ConcatenateStrings("Hello", "Platzi"); // Guardamos resultado de la operacion y le pasamos los strings
-
+            // Assert
+            Assert.NotNull(result); 
+            Assert.NotEmpty(result);    
+            // Se pueden cubrir estas dos comprobaciones ademas de la ultima
             Assert.Equal("Hello Platzi", result); // Comprobamos que la función esté devolviendo bien con Assert nos permite hacer esa comparacion, nos dice cual es el string esperado y cual es el string actual
         }
     }
